1b0384ad3c47682ac648e7608848627ac6ee982f
CarlosBGG <carlos.benavides@epn.edu.ec>
2024-11-18 22:12:43 -0500

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Encabezado from './Componentes/Encabezado';
 import ConductorMenu from './Componentes/ConductorMenu';
-import ConductorRutaInicio from './Componentes/ConductorRutaInicio';
 import ConductorRutaCheck from './Componentes/ConductorRutaCheck'; 
+import ConductorBienvenida from './Componentes/ConductorBienvenida';
+import InicioSesion from './Componentes/InicioSesion';
 import './App.css';
 
 // Opciones de menú para ConductorMenu
@@ -19,28 +20,30 @@ const conductor = {
   icono: 'https://cdn-icons-png.flaticon.com/128/1464/1464721.png',
 };
 
-//Mapa de la ruta inicial
-const maapaInicialSrc = "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d43187.25868662508!2d-78.52596115282607!3d-0.24946750913633772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x91d5a27340683463%3A0xdfb5eabd7bf000e1!2sEstacion%20Transferencia%20Ecovia%20-%20El%20Capul%C3%AD%2C%20Calle%205%2C%20Quito!3m2!1d-0.29960139999999996!2d-78.5420267!4m5!1s0x91d59a107e1cd44b%3A0x88a284f66939ed4!2sESCUELA%20POLIT%C3%89CNICA%20NACIONAL%2C%20Av.%20Ladr%C3%B3n%20de%20Guevara%20E11-253%2C%20Quito%20170143!3m2!1d-0.2124413!2d-78.4905842!5e0!3m2!1ses!2sec!4v1730851738101!5m2!1ses!2sec";
-
 //Mapa del estado de la ruta
-const mapaRutaSrc = "https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d38602.92260910906!2d-78.51359211517057!3d-0.24759756750560769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d-0.2795632!2d-78.51527229999999!4m5!1s0x91d59a107e1cd44b%3A0x88a284f66939ed4!2sESCUELA%20POLIT%C3%89CNICA%20NACIONAL%2C%20Av.%20Ladr%C3%B3n%20de%20Guevara%20E11-253%2C%20Quito%20170143!3m2!1d-0.2124413!2d-78.4905842!5e0!3m2!1ses!2sec!4v1730853412701!5m2!1ses!2sec"
+const mapaRutaSrc = "https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d40436.84340917986!2d-78.51848727862183!3d-0.22822196618896437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d-0.25927279999999997!2d-78.5028069!4m5!1s0x91d59a107e1cd44b%3A0x88a284f66939ed4!2sESCUELA%20POLIT%C3%89CNICA%20NACIONAL%2C%20Av.%20Ladr%C3%B3n%20de%20Guevara%20E11-253%2C%20Quito%20170143!3m2!1d-0.2124413!2d-78.4905842!5e0!3m2!1ses!2sec!4v1731779175048!5m2!1ses!2sec"
 
 // Lista de paradas para ConductorRutaCheck
 const paradas = ['Parada 1', 'Parada 2', 'Parada 3', 'Parada 4', 'Parada 5'];
 
-// Función para manejar el inicio de la ruta
-const iniciarRuta = () => {
-  console.log('La ruta ha sido iniciada.');
-};
 
 // Función para manejar la detención de la ruta
 const detenerRuta = () => {
-  console.log('La ruta ha sido detenida.');
+  alert('La ruta ha sido detenida.');
 };
+
+// Variable para almacenar la imagen
+const imagen = '/polibus-logo-500h.png';
+
+const mensaje = 'Bienvenido al Sistema del Transporte Estudiantil';
+
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <InicioSesion />
+      </div>
       <Encabezado />
       <div className="app-contenido">
         <ConductorMenu
@@ -49,12 +52,10 @@ function App() {
           userIcon={conductor.icono}
           menuItems={menuItems}
         />
-        <ConductorRutaInicio
-          claseContenedor="ruta-personalizada"
-          tituloRuta="RUTA: CAPULÍ"
-          mapaSrc={maapaInicialSrc}
-          textoBoton="Comenzar"
-          onIniciarRuta={iniciarRuta}
+        <ConductorBienvenida
+          mensaje={mensaje}
+          imagen={imagen}
+
         />
       </div>
       <Encabezado />
--- src/Componentes/ConductorRutaCheck.jsx
+++ src/Componentes/ConductorRutaCheck.jsx
@@ -1,39 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ConductorRutaCheck.css';
 
-class ConductorRutaCheck extends Component {
-  
-
-  render() {
-    const { titulo, claseContenedor, paradas, onBotonClick, mapaSrc } = this.props;
-
-    return (
-      <div className={`ruta-check ${claseContenedor}`}>
-        <h1 className="ruta-titulo">{titulo}</h1>
-        <div className="ruta-contenido">
-          <div className="paradas-contenedor">
+const ConductorRutaCheck = ({ titulo, claseContenedor, paradas, onBotonClick, mapaSrc }) => {
+  return (
+    <div className={`ruta-check ${claseContenedor}`}>
+      <h1 className="ruta-titulo">{titulo}</h1>
+      <div className="ruta-contenido">
+        <div className="paradas-contenedor">
           <h1>¡Paradas por las que ya pasó!</h1>
-            {paradas.map((parada, index) => (
-              <h2 key={index} className="parada">{parada}</h2>
-            ))}
-            <button className="boton-detener" onClick={onBotonClick}>
-              Detener Ruta
-            </button>
-          </div>
-          <iframe
-            src={mapaSrc}
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="mapa-google"
-          ></iframe>
+          {paradas.map((parada, index) => (
+            <div
+              key={index}
+              className={`parada ${index < 2 ? 'primera-parada' : ''}`}
+            >
+              <span>{parada}</span>
+              {index < 2 && (
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/8215/8215539.png"
+                  alt="Visto"
+                  className="parada-icono"
+                />
+              )}
+            </div>
+          ))}
+          <button className="boton-detener" onClick={onBotonClick}>
+            Detener Ruta
+          </button>
         </div>
+        <iframe
+          src={mapaSrc}
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="mapa-google"
+        ></iframe>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ConductorRutaCheck;
